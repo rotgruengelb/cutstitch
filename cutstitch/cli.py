e87a092e33393c4a534f0f7946782f8217263190
Daniel (rotgruengelb) <rotgruengelb@rotgruengelb.net>
2025-10-05 16:12:08 +0200

    fix: improve argument help text in CLI

--- cutstitch/cli.py
+++ cutstitch/cli.py
@@ -8,10 +8,10 @@ from .modes import CUTOUT_MODES
 
 def main():
     parser = argparse.ArgumentParser(description="Stitch images with configurable cutouts.")
-    parser.add_argument("files", nargs="+", help="Image files to stitch together (two or more, in order)")
-    parser.add_argument("--mode", choices=list(CUTOUT_MODES.keys()), default="vertical", help="Cutout mode (default: vertical)")
-    parser.add_argument("--angle", type=float, default=45, help="Angle for diagonal mode")
-    parser.add_argument("--output", default="stitched.png", help="Output file name")
+    parser.add_argument("files", nargs="+", help="image files to stitch together (two or more, in order)")
+    parser.add_argument("--mode", choices=list(CUTOUT_MODES.keys()), default="vertical", help="cutout mode (default: vertical)")
+    parser.add_argument("--angle", type=float, default=45, help="angle for diagonal mode")
+    parser.add_argument("--output", default="stitched.png", help="output file name")
     args = parser.parse_args()
 
     if len(args.files) < 2:
